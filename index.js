225e2c953e2f3bb426a51d8f9dffd1f4e825abef
Donna-H <donnahussey19@gmail.com>
2022-09-13 14:35:10 +0100

    express vercel

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const quotesJSONArray = require('./quotesSource.json');
+
+app.use(cors());
+
+console.log(quotesJSONArray, typeof quotesJSONArray)
+app.get('/', (req, res) => {
+  res.send('Welcome to the Mindfulness Daily Quotes API. Use the specific url endings to access quotes.')
+});
+
+app.get('/quotesList',(req, res) =>{
+  res.send({'quotesList': quotesJSONArray})
+});
+
+app.get('/randomQuote',(req, res)=>{
+    const randomNumber = Math.floor(Math.random()  *21)
+    const quoteItem = quotesJSONArray[randomNumber]
+    res.send(quoteItem)    
+});
+
+
+app.listen(process.env.PORT || 5000);
+module.exports = app;
